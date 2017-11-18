ed97c067815aa467193505672bb07416ca1e4258
abdelilah.aassou <abdelilah.aassou@check24.de>
2017-11-18 17:41:38 +0100

    First commit

---
+++ blockchain.js
@@ -0,0 +1,37 @@
+class Blockchain {
+
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "18/11/2017", "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for(let i=0; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const Block = require('./block');
+
+let block1 = new Block(0, '17/11/2017', 'hdhdhdhd');
+console.log(block1.calculateHash());
